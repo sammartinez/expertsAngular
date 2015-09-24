d401931f12adc0ad7b00397d982fd75aa1b83422
Sam Martinez & Alex Costa <student@epicodus.com>
2015-09-24 09:57:29 -0700

    Add question and answer state providers

---
+++ app.js
@@ -0,0 +1,23 @@
+var experts = angular.module('Experts', ['ui.router']);
+
+experts.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('questions', {
+    url:"/questions",
+    templateUrl:"partials/questions.html",
+    controller:'QuestionsCtrl'
+  });
+
+  $stateProvider.state('answers', {
+    url:"/answers",
+    templateUrl:"partials/answers.html",
+    controller:'AnswersCtrl'
+  });
+
+
+
+});
